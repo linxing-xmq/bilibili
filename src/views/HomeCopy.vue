9e4e678ae861b7a5b783f8f9a988fb1662a2619d
e <xiaomengqiong@outlook.com>
2020-10-18 11:39:38 +0800

    提交

---
+++ src/views/HomeCopy.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="home">
+      
+
+  </div>
+</template>
+
+<script>
+export default {
+   data(){
+       return{
+           categorylist:[],
+           active:0
+       }
+   },
+   created(){
+       
+   },
+   watch: {
+
+   },
+   methods:{
+    
+   async getCategory(){
+     const res= await this.$http.get("/category");
+     res.data.forEach((item) => {
+         item.page=0;
+         item.pagesize=10;
+         item.loading=false;
+         item.finshed=false;
+         item.list=[];
+     });
+     this.categorylist=res.data;
+     
+   },
+   selectArticle(){
+       le
+   }
+
+
+   }
+
+
+
+
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/upload.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+      <van-uploader :after-read="afterRead" />
+      
+      <van-uploader :src="imgSrc" multiple />
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            imgSrc: ''
+        }
+    },
+    methods: {
+        async afterRead(file) {
+             console.log(file);
+            const fromdata = new FormData();
+            fromdata.append('file', file.file);
+            const res =  await this.$http.post('/upload',fromdata);
+            this.imgSrc = res.data.url;
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
